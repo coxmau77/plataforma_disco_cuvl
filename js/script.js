4b6c6dd8e2146acef5896e8ce15fc1e8834a26df
Mauricio Cox <maucox77@gmail.com>
2024-04-08 19:23:42 -0300

    chore: Implementacion del ejercicio proyecto clase 08 DOM (Saludo personalizado)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-console.log("Se ejecuto script.js")
+// console.log("Se ejecuto script.js")
--- js/tours.js
+++ js/tours.js
@@ -1,1 +1,25 @@
-console.log("Se elecuto tours.js");
+// console.log("Se elecuto tours.js");
+
+let identidad = prompt('Como te identificas?\n1 - Femenino.\n2 - Masculino.\n3 - Prefiero no decirlo.');
+let nombre = prompt('Ingrese su nombre').toLowerCase();
+// let saludo = `Hola ${nombre.toUpperCase()}, como estas! Bienvenid@ a tours!`;
+
+// alert(saludo);
+
+const welcome_block = document.getElementById('welcome');
+const welcome_metaData = welcome_block.getAttribute('data-info');
+let welcome_txt = 'Bienvenid@';
+
+console.log(welcome_metaData);
+
+if (nombre.length < 2) {
+    nombre = prompt("Demasiado corto, dinos, cuál es realmente tu nombre?");
+}
+
+if(identidad != 3) {
+    identidad == 1 ? welcome_txt = 'Bienvenida' : welcome_txt = 'Bienvenido';
+}
+
+// welcome_block.textContent = `Hola ${nombre.toUpperCase()}, como estas! ${welcome_txt} a tours!`;
+welcome_block.innerHTML = `Hola <mark>${nombre.toUpperCase()}</mark> como estas! ${welcome_txt} a tours!`;
+
