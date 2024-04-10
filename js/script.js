e6677090fe7725d7ddfd68ec48690ae673dbbb7b
Mauricio Cox <maucox77@gmail.com>
2024-04-10 20:34:12 -0300

    chore: Finalizamos el ejercicio del proyecto clase 09

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 // console.log("Se ejecuto script.js")
+
--- js/tours.js
+++ js/tours.js
@@ -10,16 +10,35 @@ const welcome_block = document.getElementById('welcome');
 const welcome_metaData = welcome_block.getAttribute('data-info');
 let welcome_txt = 'Bienvenid@';
 
+let ticketIcon = document.getElementById('ticket_icon');
+
+
 console.log(welcome_metaData);
 
-if (nombre.length < 2) {
-    nombre = prompt("Demasiado corto, dinos, cuál es realmente tu nombre?");
-}
+// if (nombre.length < 2) {
+//     nombre = prompt("Demasiado corto, dinos, cuál es realmente tu nombre?");
+// }
+
+// Ejercicios 1. Say your Name Remix
 
 if(identidad != 3) {
     identidad == 1 ? welcome_txt = 'Bienvenida' : welcome_txt = 'Bienvenido';
 }
 
 // welcome_block.textContent = `Hola ${nombre.toUpperCase()}, como estas! ${welcome_txt} a tours!`;
+
+while (nombre.length < 3) {
+  nombre = prompt("Mmmm, tu nombre no puede ser tan corto, ingresa al menos 3 letras");
+}
+
+ticketIcon.classList.remove('text-gray-700');
+ticketIcon.classList.add('text-primary', 'rotar');
 welcome_block.innerHTML = `Hola <mark>${nombre.toUpperCase()}</mark> como estas! ${welcome_txt} a tours!`;
 
+// Manipular Varios Elementos - SweetAlert
+// swal({
+//     title: "Good job!",
+//     text: "You clicked the button!",
+//     icon: "success",
+//     button: "Aww yiss!",
+// });
